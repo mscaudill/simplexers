51a796a12498eb98aa279a2166b18bbb5e270ec7
mscaudill <mscaudill@gmail.com>
2024-09-10 10:30:48 -0500

    [dev, docs]: in capped sorting now handle a==b partitioning case and all funcs doced

--- src/simplexers/capped.py
+++ src/simplexers/capped.py
@@ -1,18 +1,19 @@
 """A simplexer that projects 1-D arrays onto the s-capped simplex.
 
 Functions:
-    capped_simplexer
+    capped_simplexer:
+        Computes the Euclidean projection of 1-D array(s) onto the s-capped
+        simplex.
 """
 
-from typing import Optional
 from functools import partial
+from typing import Callable, Dict
 
 import numpy as np
 import numpy.typing as npt
 from scipy import optimize
 
 from simplexers.core import arraytools
-from simplexers import positive
 
 
 def _sorting_simplexer(arr: npt.NDArray, s: int) -> npt.NDArray:
@@ -40,40 +41,46 @@ def _sorting_simplexer(arr: npt.NDArray, s: int) -> npt.NDArray:
         arXiv:1503.01002v1 [cs.LG]
     """
 
-    # FIXME need to verify operation if a == b for a vector!
     # get the number of vector components and sort them
     n = arr.shape[1]
     sorting_idxs = np.argsort(arr, axis=-1)
     z = np.take_along_axis(arr, sorting_idxs, axis=-1)
     # compute the cumulative sums of the components padding with boundary cond.
     csums = np.cumsum(z, axis=1)
-    z = arraytools.pad_along_axis(z, (0,1), constant_values=np.inf)
+    z = arraytools.pad_along_axis(z, (0, 1), constant_values=np.inf)
 
     # for each vector compute the a,b partition that satisfies KKT conditions
     result = np.zeros_like(arr)
     for idx, (y, csum) in enumerate(zip(z, csums)):
 
         for a in range(0, n):
-            # the a-1 boundary condition is -np.inf
-            low = -np.inf if a==0 else y[a-1]
+            # y[a-1] is -np.inf and csum[a-1] is 0
+            low = -np.inf if a == 0 else y[a - 1]
+            low_csum = 0 if a == 0 else csum[a - 1]
 
-            for b in range(a+1, n+1):
-                gamma = (s + b - n - csum[b-1] + csum[a-1]) / (b - a)
+            for b in range(a + 1, n + 1):
+                gamma = (s + b - n - csum[b - 1] + low_csum) / (b - a)
 
                 conditions = [
                     low + gamma <= 0,
                     y[a] + gamma > 0,
-                    y[b-1] + gamma < 1,
+                    y[b - 1] + gamma < 1,
                     y[b] + gamma >= 1,
-                    ]
+                ]
 
                 if all(conditions):
                     break
 
             else:
+                # continue on to next a
                 continue
 
+            # an a and b > a was found
             break
+
+        else:
+            # no b > a so a == b == n - s
+            a = b = n - s
 
         # add the sorted components to projection then unsort
         proj = np.zeros(n)
@@ -83,11 +90,31 @@ def _sorting_simplexer(arr: npt.NDArray, s: int) -> npt.NDArray:
 
     return result
 
-def _root_simplexer(arr: npt.NDArray, s: float, **kwargs):
-    """Computes the Euclidean projection of each 1-D array in arr onto the
-    s-capped simplex by finding the critical points of the Lagrangian.
 
-    #TODO COMPLETE
+def _root_simplexer(arr: npt.NDArray, s: float, **kwargs) -> npt.NDArray:
+    """Computes the Euclidean projection of each 1-D array in arr onto the
+    s-capped simplex via the critical points of the Lagrangian.
+
+    This is the O(n) critical point method of Ref. 1 except the Netwon-Raphson
+    method has been replaced by Brent's root finding method. This robustifies
+    the algorithm because there is no need for a second derivative and its
+    associated zero-division problem in the non-feasible region. Please note
+    that Brent's method requires a bracketing interval [a,b] such that w', the
+    Lagrangian's derivative, has opposite signs at the interval's endpoints:
+    sign(w'(a)) = -1 * sign(w'(b)). This is satisfied if the interval is chosen
+    to be [min(vector) - 1, max(vector)] for each vector in arr (see Ref 1 Eqn
+    6).
+
+    Args:
+        arr:
+            A 2-D numpy array of vectors to project onto the s-capped simplex.
+        s:
+            The sum constraint for each vector.
+        kwargs:
+            Any valid keyword only arguments for scipy.optimize.brentq function
+
+    Returns:
+        A 2-D array of vector projections one per vector in arr.
     """
 
     gamma_lows = np.min(arr, axis=1) - 1
@@ -102,12 +129,12 @@ def _root_simplexer(arr: npt.NDArray, s: float, **kwargs):
 
     result = np.zeros_like(arr)
     for idx, (bracket, y) in enumerate(zip(brackets, arr)):
-        #print(idx)
         func = partial(omega_prime, y=y, s=s)
         gamma_star = optimize.brentq(func, *bracket, **kwargs)
         result[idx] = np.minimum(1, np.maximum(y - gamma_star, 0))
 
     return result
+
 
 def capped_simplexer(
     arr: npt.NDArray,
@@ -118,6 +145,14 @@ def capped_simplexer(
 ) -> npt.NDArray:
     """Computes the Euclidean projection of each 1-D array in arr onto the
     s-capped simplex.
+
+    The s-capped simplex projection locates the vector x closest to vector
+    y subject to a component constraint and sum constraint:
+
+    minimize {||x - y||**2 subject to 0 <= x_i <= 1 and sum(x_i) = s}
+
+    This method finds this solution by locating the Lagrangian's critical points
+    (Ref. 1) or the sort algorithm (Ref. 2).
 
     Args:
         arr:
@@ -132,9 +167,9 @@ def capped_simplexer(
             projection. Must be one of {'root', 'sort'}. The 'root' method finds
             the roots of the derivative of the Lagrangian using Brents method.
             This method is O(n) where n is the number of vector components to
-            project (See Reference 1). 'Sort' uses the O(n**2) sorting algorithm
-            of Reference 2. For vectors with very few components it will have a
-            speed advantage.
+            project (See Ref. 1). 'Sort' uses the O(n**2) sorting algorithm
+            (Ref. 2). For vectors with very few components it will have a speed
+            advantage.
         kwargs:
             Any valid keyword only arguments for scipy.optimize.brentq function
 
@@ -152,13 +187,15 @@ def capped_simplexer(
            arXiv:1503.01002v1 [cs.LG]
     """
 
-    methods = {'sort': _sorting_simplexer, 'root': _root_simplexer}
-
-    algorithm = methods[method]
-
     if arr.ndim > 2:
         msg = 'Array(s) to project must have at most 2 dimensions'
         raise ValueError(msg)
+
+    methods: Dict[str, Callable] = {
+        'sort': _sorting_simplexer,
+        'root': _root_simplexer,
+    }
+    algorithm = methods[method]
 
     z = np.atleast_2d(arr)
     z = z.T if axis == 0 else z
@@ -167,14 +204,13 @@ def capped_simplexer(
     return result.T if axis == 0 else result
 
 
-
-
 if __name__ == '__main__':
 
     import time
 
+    """
     rng = np.random.default_rng(0)
-    y = rng.random((4, 1000)) - 0.5
+    y = rng.random((3, 4)) - 0.5
 
     t0 = time.perf_counter()
     target = positive._sorting_simplexer(y, s=1, axis=0)
@@ -188,5 +224,9 @@ if __name__ == '__main__':
     res_root = capped_simplexer(y, s=1, axis=0, method='root')
     print('Root Simplexer Time: ', time.perf_counter() - t0)
 
-    #print(np.allclose(res_sort, target))
+    print(np.allclose(res_sort, target))
     print(np.allclose(res_root, target))
+    """
+
+    q = np.atleast_2d(np.array([0, 0, 0, 0, 0, 2, 1], dtype=float))
+    test = _sorting_simplexer(q, s=2)
